03d0949817593edb580049f23e015c7141950ebe
IgnacioRezet <imunozm25@gmail.com>
2024-06-26 00:09:06 -0400

    fix css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ListadoTareas from "./ListadoTareas"
 import Header from "./Header"
 import React, { useEffect, useState } from 'react'
 import { useFetchTask } from "./hooks/useFetchTask";
+import './assets/css/app.css';
 
 
 
